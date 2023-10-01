a77a4c028efbc25e3a567ef18d2ee57820a2ef29
Judy942 <109392364+Judy942@users.noreply.github.com>
2023-10-01 10:53:23 +0700

    10h53

--- lib/core/home/home/home_navigation_screen.dart
+++ lib/core/home/home/home_navigation_screen.dart
@@ -26,203 +26,212 @@ class _HomeNavigationScreenState extends State<HomeNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 40),
-                child: Image.asset(ImageConstant.splashLogo),
-              ),
-              Positioned(
-                bottom: 10,
-                right: 20,
-                left: 20,
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: 60,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        BoxShadow(
-                          color: ColorConstant.primaryColor.withOpacity(0.2),
-                          blurRadius: 16,
-                          offset: const Offset(1, 1),
-                        ),
-                      ]),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                    ),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          ImageConstant.logoApp,
-                          height: 80,
-                        ),
-                        const SizedBox(
-                          width: 16,
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              "Waste Classification",
-                              style: TextStyle(
-                                  fontSize: 14, fontWeight: FontWeight.w600),
-                            ),
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text(
-                              "The classification of waste",
-                              style: TextStyle(
-                                  fontSize: 12, fontWeight: FontWeight.w400),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: Image.asset(ImageConstant.splashLogo),
                 ),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: List.generate(
-                  mapMenu.length,
-                  (index) => GestureDetector(
-                        onTap: () {
-                          if (mapMenu.values.elementAt(index) ==
-                              "search") {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => const QRGenerate()));
-                          } else if (mapMenu.values.elementAt(index) ==
-                              "quizz") {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => const CultivationScreen()));
-                          } else {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => const PengolahanScreen()));
-                          }
-                        },
-                        child: SizedBox(
-                          width: 60,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Container(
-                                width: 60,
-                                height: 60,
-                                margin: const EdgeInsets.only(bottom: 10),
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(8),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: ColorConstant.primaryColor
-                                            .withOpacity(0.1),
-                                        blurRadius: 10,
-                                        offset: const Offset(0, 3),
-                                      ),
-                                    ]),
-                                child: Image(
-                                    width: 32,
-                                    height: 32,
-                                    image: Svg(mapMenu.keys.elementAt(index))),
+                Positioned(
+                  bottom: 10,
+                  right: 20,
+                  left: 20,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: ColorConstant.primaryColor.withOpacity(0.2),
+                            blurRadius: 16,
+                            offset: const Offset(1, 1),
+                          ),
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                      ),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            ImageConstant.logoApp,
+                            height: 80,
+                          ),
+                          const SizedBox(
+                            width: 16,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                "Waste Classification",
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w600),
+                              ),
+                              SizedBox(
+                                height: 4,
                               ),
                               Text(
-                                mapMenu.values.elementAt(index),
-                                maxLines: 2,
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                    fontSize: 13, fontWeight: FontWeight.w500),
+                                "The classification of waste",
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w400),
                               )
                             ],
-                          ),
-                        ),
-                      )),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Divider(
-            thickness: 10,
-            color: Colors.grey.withOpacity(0.1),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-            child: 
-            // Text(
-            //   "Wellcome to Waste Classification",
-            //   style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-            // ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                // const Text(
-                //   "Hello",
-                //   style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                // ),
-                SizedBox(
-                  height: 10,
-                ),
-
-                Text(
-                  """Wellcome to
-
-        Waste Classification""",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                ),
-                // GestureDetector(
-                //   onTap: () {
-                //     Navigator.of(context).push(MaterialPageRoute(
-                //         // builder: (context) => const ArticleScreen()));
-                //         builder: (context) => const Text("Wellcome to Waste Classification")));
-                //   },
-                //   child: const Text(
-                //     "Lihat Semua",
-                //     style: TextStyle(
-                //         fontSize: 14,
-                //         fontWeight: FontWeight.w400,
-                //         color: ColorConstant.primaryColor),
-                //   ),
-                // )
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
-          ),
-          // ListView.builder(
-          //   padding: const EdgeInsets.only(bottom: 24),
-          //   shrinkWrap: true,
-          //   physics: const NeverScrollableScrollPhysics(),
-          //   itemCount: 5,
-          //   itemBuilder: ((context, index) {
-          //     return GestureDetector(
-          //       onTap: () {
-          //         Navigator.of(context).push(MaterialPageRoute(
-          //             builder: (_) => ArticleDetailScreen(
-          //                 source: _information[index].sumber)));
-          //       },
-          //       child: CardArtikel(
-          //         data: _information[index],
-          //       ),
-          //     );
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: List.generate(
+                    mapMenu.length,
+                    (index) => GestureDetector(
+                          onTap: () {
+                            if (mapMenu.values.elementAt(index) == "search") {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const QRGenerate()));
+                            } else if (mapMenu.values.elementAt(index) ==
+                                "quizz") {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) =>
+                                      const CultivationScreen()));
+                            } else {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) =>
+                                      const PengolahanScreen()));
+                            }
+                          },
+                          child: SizedBox(
+                            width: 60,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  margin: const EdgeInsets.only(bottom: 10),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(8),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: ColorConstant.primaryColor
+                                              .withOpacity(0.1),
+                                          blurRadius: 10,
+                                          offset: const Offset(0, 3),
+                                        ),
+                                      ]),
+                                  child: Image(
+                                      width: 32,
+                                      height: 32,
+                                      image:
+                                          Svg(mapMenu.keys.elementAt(index))),
+                                ),
+                                Text(
+                                  mapMenu.values.elementAt(index),
+                                  maxLines: 2,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500),
+                                )
+                              ],
+                            ),
+                          ),
+                        )),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Divider(
+              thickness: 10,
+              color: Colors.grey.withOpacity(0.1),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+              child:
+                  // Text(
+                  //   "Wellcome to Waste Classification",
+                  //   style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  // ),
+                  Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  // const Text(
+                  //   "Hello",
+                  //   style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  // ),
+                  SizedBox(
+                    height: 10,
+                  ),
+
+                  Text(
+                    "Wellcome to",
+                    
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Waste Classification",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                  // GestureDetector(
+                  //   onTap: () {
+                  //     Navigator.of(context).push(MaterialPageRoute(
+                  //         // builder: (context) => const ArticleScreen()));
+                  //         builder: (context) => const Text("Wellcome to Waste Classification")));
+                  //   },
+                  //   child: const Text(
+                  //     "Lihat Semua",
+                  //     style: TextStyle(
+                  //         fontSize: 14,
+                  //         fontWeight: FontWeight.w400,
+                  //         color: ColorConstant.primaryColor),
+                  //   ),
+                  // )
+                ],
+              ),
+            ),
+            // ListView.builder(
+            //   padding: const EdgeInsets.only(bottom: 24),
+            //   shrinkWrap: true,
+            //   physics: const NeverScrollableScrollPhysics(),
+            //   itemCount: 5,
+            //   itemBuilder: ((context, index) {
+            //     return GestureDetector(
+            //       onTap: () {
+            //         Navigator.of(context).push(MaterialPageRoute(
+            //             builder: (_) => ArticleDetailScreen(
+            //                 source: _information[index].sumber)));
+            //       },
+            //       child: CardArtikel(
+            //         data: _information[index],
+            //       ),
+            //     );
             // }),
-          // )
-        ],
-      ),
+            // )
+          ],
+        ),
       ),
     );
   }
